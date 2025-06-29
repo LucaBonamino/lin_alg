b07868892aed58f7ab40cbcfed621a9fab752094
LucaBonamino <luca.bonamino@hotmail.com>
2025-06-30 00:10:09 +0200

    tests

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,9 @@ mod tests {
         let mat = gf2_matrix::GF2Matrix::new(vec![vec![1,0], vec![1,1]]);
         let (ech_form, _) = mat.echelon_form();     
         assert_eq!(ech_form.elements, vec![vec![1,0], vec![0,1]]);
+        let mat = gf2_matrix::GF2Matrix::new(vec![vec![1,0,0,0], vec![0,1,0,1], vec![0,1,0,1]]);
+        let (ech_form, _) = mat.echelon_form();     
+        assert_eq!(ech_form.elements, vec![vec![1,0,0,0], vec![0,1,0,1], vec![0,0,0,0]]);
    
     }
 
@@ -28,5 +31,51 @@ mod tests {
         let (_, ops) = mat.echelon_form();
         assert_eq!(ops, vec![(1,0)])
     }
+
+    #[test]
+    fn ncols_nrows(){
+        let mat = gf2_matrix::GF2Matrix::new(vec![vec![1,0,1], vec![1,1,0]]);
+        assert_eq!(mat.nrows(), 2);
+        assert_eq!(mat.ncols(), 3)
+    }
+
+    #[test]
+    fn get_pivot(){
+        assert_eq!(gf2_matrix::GF2Matrix::get_pivot(&vec![1,0,0,1]).unwrap(), 0);
+        assert_eq!(gf2_matrix::GF2Matrix::get_pivot(&vec![0,1,1,0]).unwrap(), 1);
+        assert_eq!(gf2_matrix::GF2Matrix::get_pivot(&vec![0,0,1,1]).unwrap(), 2);
+        assert_eq!(gf2_matrix::GF2Matrix::get_pivot(&vec![0,0,0,1]).unwrap(), 3);
+        assert!(gf2_matrix::GF2Matrix::get_pivot(&vec![0,0,0,0]).is_none());
+    }
+
+    #[test]
+    fn is_reduced_echelon(){
+        assert_eq!(gf2_matrix::GF2Matrix::new(vec![vec![1,0,0,0], vec![1,1,0,1]]).is_reduced_echelon(), false);
+        assert_eq!(gf2_matrix::GF2Matrix::new(vec![vec![1,0,0,0], vec![1,1,0,1]]).is_reduced_echelon(), false);
+        assert_eq!(gf2_matrix::GF2Matrix::new(vec![vec![1,1,0,1], vec![0,1,0,1]]).is_reduced_echelon(), false);
+        assert_eq!(gf2_matrix::GF2Matrix::new(vec![vec![1,0,0,1], vec![0,1,0,1]]).is_reduced_echelon(), true);
+        assert_eq!(gf2_matrix::GF2Matrix::new(vec![vec![1,0,0,0], vec![1,1,0,1], vec![0,0,1,1]]).is_reduced_echelon(), false);
+        assert_eq!(gf2_matrix::GF2Matrix::new(vec![vec![1,0,0,0], vec![0,1,0,0], vec![0,0,1,1]]).is_reduced_echelon(), true);
+        assert_eq!(gf2_matrix::GF2Matrix::new(vec![vec![1,0,1,0], vec![0,0,0,0]]).is_reduced_echelon(), true);
+        assert_eq!(gf2_matrix::GF2Matrix::new(vec![vec![1,0,0,0], vec![0,1,0,1], vec![0,1,0,1]]).is_reduced_echelon(), false);
+
+    }
+
+    #[test]
+    fn rank(){
+        assert_eq!(gf2_matrix::GF2Matrix::new(vec![vec![1,0,0,0], vec![0,1,0,1]]).rank(), 2);
+        assert_eq!(gf2_matrix::GF2Matrix::new(vec![vec![1,0,0,0], vec![1,0,0,0]]).rank(), 1);
+    }
+
+    #[test]
+    fn image(){
+        let mat_1 = gf2_matrix::GF2Matrix::new(vec![vec![1,0,0,0], vec![0,1,0,1]]);
+        assert_eq!(mat_1.image(), mat_1.elements);
+        let mat_2 = gf2_matrix::GF2Matrix::new(vec![vec![1,0,0,0], vec![0,1,0,1], vec![0,1,0,1]]);
+        assert_eq!(mat_2.image(), mat_1.elements);
+        let mat = gf2_matrix::GF2Matrix::new(vec![vec![1,0,0,0], vec![0,0,0,0]]);
+        assert_eq!(mat.image(), vec![vec![1,0,0,0]]);
     
+    }
+
 }
